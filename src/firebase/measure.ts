1a906ae6939359c7cb9425f70548745ed336e469
Simon Green <simon@captaincodeman.com>
2024-05-30 22:06:27 -0600

    fix use of Buffer on client

--- src/firebase/measure.ts
+++ src/firebase/measure.ts
@@ -13,7 +13,7 @@ function objectSize(obj: any): number {
 
   switch (type) {
     case 'string':
-      return Buffer.byteLength(obj) + 1
+      return byteLength(obj) + 1
 
     case 'boolean':
       return 1
@@ -55,4 +55,16 @@ function objectSize(obj: any): number {
     default:
       throw 'unexpected type ' + type
   }
+}
+
+function byteLength(str: string) {
+	// returns the byte length of an utf8 string
+	var s = str.length
+	for (var i = str.length - 1; i >= 0; i--) {
+		var code = str.charCodeAt(i)
+		if (code > 0x7f && code <= 0x7ff) s++
+		else if (code > 0x7ff && code <= 0xffff) s += 2
+		if (code >= 0xdc00 && code <= 0xdfff) i-- //trail surrogate
+	}
+	return s
 }
